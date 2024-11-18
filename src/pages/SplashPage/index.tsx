435af919cb7c917d7f9ca558ef93a25dab3cc427
avangardewashere <avelpanaligan@gmail.com>
2024-11-18 16:50:37 +0800

    add/spalsh-intro

--- src/pages/SplashPage/index.tsx
+++ src/pages/SplashPage/index.tsx
@@ -1,18 +1,23 @@
-import { memo } from 'react';
-import clsx from 'clsx';
-import style from './index.module.scss';
+import { memo } from "react";
+import clsx from "clsx";
+import style from "./index.module.scss";
 import logo from "./../../assets/images/splash/plan-it.png";
-import splashBg from "./../../assets/images/splash/splashBG.png"
+import splashBg from "./../../assets/images/splash/splashBG.png";
 const Splash = memo(() => {
   return (
-	<div className={clsx(style.container)}>
-	 <div className={style.logo}>
-		<img src={logo} alt="plan-it-logo" />
-		<span>PlanIT</span>
-	 </div>
-	 <img className={clsx(style.splashBg)} src={splashBg} alt="splash-background-plan-it" />
-	  
-	</div>
+    <div className={clsx(style.container)}>
+      <div className={clsx(style.splashIntro)}>
+        <div className={style.logo}>
+          <img src={logo} alt="plan-it-logo" />
+          <span>PlanIT</span>
+        </div>
+        <img
+          className={clsx(style.splashBg)}
+          src={splashBg}
+          alt="splash-background-plan-it"
+        />
+      </div>
+    </div>
   );
 });
 
