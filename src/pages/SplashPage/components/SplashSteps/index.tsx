ba67ce506cc931d6a9a4b9536b5f13477ba08ddc
avangardewashere <avelpanaligan@gmail.com>
2024-11-19 13:28:03 +0800

    design/swiper-splash

--- src/pages/SplashPage/components/SplashSteps/index.tsx
+++ src/pages/SplashPage/components/SplashSteps/index.tsx
@@ -7,16 +7,19 @@ interface SplashSteps {
 	imgSrc?:string;
 	capTitle?:string;
 	capDetails?:string;
+	imgWidth?:number;
+	imgHeight?:number;
 }
 
 
 const SplashSteps = memo((props:SplashSteps) => {
-	const {imgSrc,capTitle,capDetails} = props;
+	const {imgSrc,capTitle,capDetails,imgWidth,imgHeight} = props;
   return (
     <div className={clsx(style.container)}>
       <img
         className={clsx(style.illustration)}
         src={imgSrc || illusImg}
+		style={{width:imgWidth || 0,height:imgHeight || 0}}
         alt="plan-it-illustration"
       />
       <div className={clsx(style.caption)}>
--- src/pages/SplashPage/components/SplashSwiper/index.tsx
+++ src/pages/SplashPage/components/SplashSwiper/index.tsx
@@ -11,18 +11,24 @@ const SplashSwiper = memo(() => {
   const splashSwipeData = [
     {
       imgSrc: iluOne,
+	  imgWidth:350,
+	  imgHeight:337,
       capDetails:
         "Change your life by slowly adding new healty habits and sticking to them.",
       capTitle: "Create Good Habits",
     },
     {
       imgSrc: iluTwo,
+	  imgWidth:300,
+	  imgHeight:320,
       capDetails:
         "Everyday you become one step closer to your goal. Don’t give up!",
       capTitle: "Track Your Progress",
     },
     {
       imgSrc: iluThree,
+	  imgWidth:390,
+	  imgHeight:300,
       capDetails:
         "Find friends to discuss common topics. Complete challenges together.",
       capTitle: "Stay Together and Strong",
@@ -31,13 +37,15 @@ const SplashSwiper = memo(() => {
 
   return (
     <div className={clsx(style.container)}>
-      <Swiper>
+      <Swiper className={clsx(style.splashSwiper)}>
         {splashSwipeData.map((item, index) => (
           <Swiper.Item key={index}>
             <SplashSteps
               imgSrc={item.imgSrc}
               capDetails={item.capDetails}
               capTitle={item.capTitle}
+			  imgWidth={item.imgWidth}
+			  imgHeight={item.imgHeight}
             />
           </Swiper.Item>
         ))}
