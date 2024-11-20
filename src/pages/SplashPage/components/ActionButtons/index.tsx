21afb66ef70dd2bd789bef68f2402d8f3f214a95
avangardewashere <avelpanaligan@gmail.com>
2024-11-20 12:01:25 +0800

    add/action-buttons-data-simplify-map-code

--- src/pages/SplashPage/components/ActionButtons/index.tsx
+++ src/pages/SplashPage/components/ActionButtons/index.tsx
@@ -2,27 +2,56 @@ import { memo } from "react";
 import clsx from "clsx";
 import style from "./index.module.scss";
 import LogButton from "./button";
+import emailImg from "./../../../../assets/images/splash/emailLogin.png";
+import appleImg from "./../../../../assets/images/splash/Apple.png";
+import googleImg from "./../../../../assets/images/splash/GoogleIcon.png";
+import facebookImg from "./../../../../assets/images/splash/facebook.png";
 
 const SplashActions = memo(() => {
+
+  const actionData = [
+    {
+      id: 101,
+      text: "Continue with Email",
+      imgSrc: emailImg,
+      action: () => {
+        console.log("hello");
+      },
+    },
+    {
+      id: 102,
+      text: "Apple ID",
+      imgSrc: appleImg,
+      action: () => {
+        console.log("hello");
+      },
+    },
+    {
+      id: 102,
+      text: "Google",
+      imgSrc: googleImg,
+      action: () => {
+        console.log("hello");
+      },
+    },
+    {
+      id: 101,
+      text: "Facebook",
+      imgSrc: facebookImg,
+      action: () => {
+        console.log("hello");
+      },
+    },
+  ];
+
   return (
     <div className={clsx(style.container)}>
-      <div className={clsx(style.mainButton)}>
-        <LogButton text="Continue with E-mail" />
-      </div>
-      <div className={clsx(style.subButtons)}>
-        <div className={clsx(style.methodLogin)}>
-          <LogButton text="Apple-ID" />
-        </div>
-        <div className={clsx(style.methodLogin)}>
-          <LogButton text="FaceBook" />
-        </div>
-        <div className={clsx(style.methodLogin)}>
-          <LogButton text="Google" />
-        </div>
-        {/* 
-			
-
-			? */}
+      <div className={clsx(style.splashActions)}>
+        {actionData?.map((item, index) => {
+          return (
+            <LogButton key={index} imgSrc={item.imgSrc} text={item.text} />
+          );
+        })}
       </div>
     </div>
   );
