fd4267aacd893fccded3ee14d2cb0ec4bebf97d3
avangardewashere <avelpanaligan@gmail.com>
2024-11-20 18:45:19 +0800

    added/navigations

--- src/component/SectionName/index.tsx
+++ src/component/SectionName/index.tsx
@@ -1,23 +1,31 @@
 import { memo } from "react";
 import clsx from "clsx";
 import style from "./index.module.scss";
-
+import chevImg from "./../../assets/images/common/chevron.png"
 interface SectionNameProps {
   section?: string;
   viewAll?: () => void;
+  navBack?: () => void;
 }
 
 const SectionName: React.FC<SectionNameProps> = memo((props) => {
-  const { section, viewAll } = props;
+  const { section, viewAll,navBack } = props;
 
   const handleViewAll = () => {
     if (!viewAll) return;
 
-    viewAll();
+    viewAll?.();
   };
+
+  const handleNavigateBack = () =>{
+    navBack?.()
+  }
 
   return (
     <div className={clsx(style.container)}>
+      <div className={clsx(style.chevron)} onClick={handleNavigateBack}>
+        <img src={chevImg} alt="plan-it-back-button" />
+      </div>
       <span>{section}</span>
       {viewAll && (
         <div className={clsx(style.viewAll)} onClick={handleViewAll}>
--- src/pages/Authpage/index.tsx
+++ src/pages/Authpage/index.tsx
@@ -2,11 +2,18 @@ import { memo } from 'react';
 import clsx from 'clsx';
 import style from './index.module.scss';
 import SectionName from '../../component/SectionName';
+import { useNavigate } from 'react-router-dom';
 
 const AuthPage = memo(() => {
+	const navigate = useNavigate();
+	const handleNavBack = () =>{
+		navigate("/")
+	}
+
   return (
 	<div className={clsx(style.container)}>
-	  <SectionName section='Continue with Email' />
+	  <SectionName navBack={handleNavBack} section='Continue with Email' />
+	  hi
 	</div>
   );
 });
