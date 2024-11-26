ad4f595e3359fdce12634ab125a2c078cf1e6614
avangardewashere <avelpanaligan@gmail.com>
2024-11-26 12:20:27 +0800

    start/creating-card

--- src/pages/Authpage/Creation/card/index.tsx
+++ src/pages/Authpage/Creation/card/index.tsx
@@ -1,29 +1,28 @@
 import { memo, useState } from "react";
 import clsx from "clsx";
 import style from "./index.module.scss";
-import sample from "./../../../../assets/images/footer/discover.png"
+import sample from "./../../../../assets/images/footer/discover.png";
 import { useMemoizedFn } from "ahooks";
 
 interface CardProps {
-	imgSrc?:string;
-	cardName?:string;
-	isSelected?:boolean;
-	select?:(data:boolean )=>void;
+  imgSrc?: string;
+  cardName?: string;
+  isSelected?: boolean;
+  select?: (data: boolean) => void;
 }
 
-const Card = memo((props:CardProps) => {
-	const {imgSrc,cardName,isSelected,select} = props;
-	const [selected,setSelected] = useState(false);
-	const handleSelection = useMemoizedFn(()=>{
-		setSelected(!selected);
+const Card = memo((props: CardProps) => {
+  const { imgSrc, cardName, isSelected, select } = props;
+  const [selected, setSelected] = useState(false);
+  const handleSelection = useMemoizedFn(() => {
+    setSelected(!selected);
 
-		select?.(isSelected ?? false)
-
-	})
+    select?.(isSelected ?? false);
+  });
   // 160 x 134 sixe
   return (
     <div onClick={handleSelection} className={clsx(style.container)}>
-		<img src={imgSrc ?? sample} alt="" />
+      <img src={imgSrc ?? sample} alt="" />
       <span>{cardName ?? "Card"}</span>
     </div>
   );
